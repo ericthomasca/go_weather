1369cf388ddb1538bd8233a06de4aa53891b62dd
Eric Thomas <eric@ericthomas.ca>
2022-09-19 21:52:54 -0230

    start of project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ericthomas.ca/go_weather
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	
+}
+
+// use godot package to load/read the .env file and
+// return the value of the key
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+  
+	if err != nil {
+	  log.Fatalf("Error loading .env file")
+	}
+  
+	return os.Getenv(key)
+  }
+  
