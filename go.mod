85a3f96fa0bf61e8a59ec461d083375930523e2a
Eric Thomas <eric@ericthomas.ca>
2022-09-19 23:28:49 -0230

    successful pull json from api

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ericthomas.ca/go_weather
 
 go 1.19
+
+require github.com/joho/godotenv v1.4.0
--- main.go
+++ main.go
@@ -1,26 +1,97 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"strings"
+
 	"github.com/joho/godotenv"
 )
 
-func main() {
-	
+type WeatherData struct {
+	Coord struct {
+		Lon float64 `json:"lon"`
+		Lat float64 `json:"lat"`
+	} `json:"coord"`
+	Weather []struct {
+		ID          int    `json:"id"`
+		Main        string `json:"main"`
+		Description string `json:"description"`
+		Icon        string `json:"icon"`
+	} `json:"weather"`
+	Base string `json:"base"`
+	Main struct {
+		Temp      float64 `json:"temp"`
+		FeelsLike float64 `json:"feels_like"`
+		TempMin   float64 `json:"temp_min"`
+		TempMax   float64 `json:"temp_max"`
+		Pressure  int     `json:"pressure"`
+		Humidity  int     `json:"humidity"`
+		SeaLevel  int     `json:"sea_level"`
+		GrndLevel int     `json:"grnd_level"`
+	} `json:"main"`
+	Visibility int `json:"visibility"`
+	Wind       struct {
+		Speed float64 `json:"speed"`
+		Deg   int     `json:"deg"`
+		Gust  float64 `json:"gust"`
+	} `json:"wind"`
+	Clouds struct {
+		All int `json:"all"`
+	} `json:"clouds"`
+	Dt  int `json:"dt"`
+	Sys struct {
+		Type    int    `json:"type"`
+		ID      int    `json:"id"`
+		Country string `json:"country"`
+		Sunrise int    `json:"sunrise"`
+		Sunset  int    `json:"sunset"`
+	} `json:"sys"`
+	Timezone int    `json:"timezone"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Cod      int    `json:"cod"`
 }
 
-// use godot package to load/read the .env file and
-// return the value of the key
-func goDotEnvVariable(key string) string {
+func init() {
 
-	// load .env file
 	err := godotenv.Load(".env")
   
 	if err != nil {
 	  log.Fatalf("Error loading .env file")
 	}
-  
-	return os.Getenv(key)
   }
+
+func main() {
+	args := os.Args
+	
+	api_key := os.Getenv("OPEN_WEATHER_MAP_API")
+	zip_code := strings.ToUpper(args[1])
+	country_code := strings.ToUpper(args[2])
+
+	api_url := "https://api.openweathermap.org/data/2.5/weather?appid=" + api_key + "&zip=" + zip_code + "," + country_code
+
+	response, err := http.Get(api_url)
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(responseData))
+
+	
+}
+
+
+
   
